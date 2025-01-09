ce4a27531e82dcb06c3e9c9f5fc28d833c103c65
o.sustam <oben.sustam@streicher.de>
2025-01-09 15:04:22 +0100

    add language and icon packages and add App.vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <RosWorld></RosWorld>
+</template>
+  
+<script>
+  import RosWorld from './components/RosWorld.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+        RosWorld
+    }
+  }
+</script>
+  
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
-import App from './RosWorld.vue'
+import App from './App.vue'
+import i18n from './i18n/i18n';  // Import i18n configuration
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
 createApp(App)
+  .use(i18n)  // Use i18n in your app
   .use(vuetify)
   .mount('#app')
